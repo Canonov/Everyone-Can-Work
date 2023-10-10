7c1e193996fde7d0358435fd8d562a2e06b00703
CanonOverseer <agent6841@gmail.com>
2023-10-10 15:06:55 +0800

    C# Patch Version

---
+++ Source/EveryoneCanWork/LifeStageWorkSettings_IsDisabled_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+
+// ReSharper disable RedundantAssignment
+// ReSharper disable InconsistentNaming
+
+namespace EveryoneCanWork
+{
+    [HarmonyPatch(typeof(LifeStageWorkSettings), "IsDisabled")]
+    public class LifeStageWorkSettings_IsDisabled_Patch
+    {
+        private static void Postfix(ref bool __result)
+        {
+            __result = false;
+        }
+    }
+}
--- Source/EveryoneCanWork/StartupPatcher.cs
+++ Source/EveryoneCanWork/StartupPatcher.cs
@@ -1,10 +1,17 @@
-﻿using Verse;
+﻿using HarmonyLib;
+using Verse;
 
 namespace EveryoneCanWork
 {
     [StaticConstructorOnStartup]
     public class StartupPatcher
     {
+
+        static StartupPatcher()
+        {
+            var harmony = new Harmony("com.EveryoneCanWork.patches");
+            harmony.PatchAll();
+        }
         
     }
 }
